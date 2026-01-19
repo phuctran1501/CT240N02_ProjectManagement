37a03c322b00bb385fd3cbc946d484eef7f9fe96
Nguyen Loc <locdedoda@gmail.com>
2026-01-19 17:38:19 +0700

    refactor: clear all files

--- src/main/java/com/projectmanager/projectmanagement/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.projectmanager.projectmanagement;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.projectmanager.projectmanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.projectmanager.projectmanagement to javafx.fxml;
-    exports com.projectmanager.projectmanagement;
-}
